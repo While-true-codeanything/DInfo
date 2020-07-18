aefb631ebc075045e20662024f6a29c687ec7068
While-true-codeanything <pavlov.erg@gmail.com>
2020-07-18 13:28:19 +0300

    Added supposed way of getting coordinates

--- app/src/main/java/com/example/dinfo/MainActivity.kt
+++ app/src/main/java/com/example/dinfo/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.dinfo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var loc: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(
+                this@MainActivity, "Smth went wrong",
+                Toast.LENGTH_LONG
+            ).show()
+            // Здесь будет обработка ошибок
+            // Надо добавить проверку на интернет
+            return
+        } else loc.requestSingleUpdate(LocationManager.GPS_PROVIDER, listener, null)
+    }
+
+    private val listener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location?) {
+            Toast.makeText(
+                this@MainActivity,
+                "Широта:" + location?.latitude.toString() + "\n Долгота:" + location?.longitude.toString(),
+                Toast.LENGTH_LONG
+            ).show()
+            //Здесь будет получение данных
+            //Необходимо добавить диалог на разрешение получения местоположения
+
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 }
