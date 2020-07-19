6f3d61d7df9a918393d235c9e8ed2f95f03480c1
userTVI <veronika.terekhova.2003@gmail.com>
2020-07-20 02:18:10 +0300

    Added check for internet connection

---
+++ app/src/main/java/com/example/dinfo/ConnectionCheckActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dinfo
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_connection_check.*
+
+class ConnectionCheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection_check)
+
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        if (isConnected) startActivity(Intent(this, MainActivity::class.java))
+        else Toast.makeText(this, "Нет подключения к сети. Попробуйте еще раз.",
+            Toast.LENGTH_SHORT).show()
+
+        button2.setOnClickListener { this.recreate() }
+    }
+}
