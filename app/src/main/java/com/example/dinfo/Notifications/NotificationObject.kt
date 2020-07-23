8e04c1a86698e6f83b34f5dc33272dc607fd5275
While-true-codeanything <pavlov.erg@gmail.com>
2020-07-23 15:47:46 +0300

    small fix

---
+++ app/src/main/java/com/example/dinfo/Notifications/NotificationObject.kt
@@ -0,0 +1,12 @@
+package com.example.dinfo.Notifications
+
+import com.example.dinfo.CurrencydateItem
+
+class NotificationObject(var head:String,var body:String, var dateon:String,var datefrom:String){
+    companion object{
+        fun fromString(st:String): NotificationObject = NotificationObject(st.split("- -")[0],st.split("- -")[1],st.split("- -")[2],st.split("- -")[3])
+    }
+    override fun toString(): String {
+        return "$head- -$body- -$dateon- -$datefrom"
+    }
+}
